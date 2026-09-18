28eef080764e5aae5efe7b4c07191fe11b64f6db
Tr43212 <trinhtuantruong2k1@gmail.com>
2026-09-18 21:53:46 +0700

    updet

--- refresh.sh
+++ refresh.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 
+set -xe
+
 PWD=$(pwd)
 cd ~/.local/new-dotfiles
 
@@ -12,6 +14,7 @@ cp -r ~/Scripts/ ./Scripts/
 cp -r ~/Documents/wallpaper.jpeg ./wallpaper.jpeg
 cp -r ~/.Xresources ./.Xresources
 cp -r /etc/environment ./etc/environment
+cp -r /etc/mkinitcpio.d/linux-zen.preset ./etc/mkinitcpio.d/linux-zen-preset
 
 cp -r ~/.emacs.d/init.el ./.emacs.d/init.el
 cp -r ~/.emacs.d/custom.el ./.emacs.d/custom.el
