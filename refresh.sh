53739df185646601c16acbfe3ab2cc213709484d
Tr43212 <trinhtuantruong2k1@gmail.com>
2026-09-18 21:17:35 +0700

    updet

--- refresh.sh
+++ refresh.sh
@@ -6,7 +6,7 @@ cp -r ~/.config/polybar/ ./polybar/
 cp -r ~/.config/qtile/ ./qtile/
 
 cp -r ~/Scripts/ ./Scripts/
-cp -r ~/Documents/wallpaper.jpeg 
+cp -r ~/Documents/wallpaper.jpeg ./wallpaper.jpeg
 
 cp -r ~/.emacs.d/init.el ./.emacs.d/init.el
 cp -r ~/.emacs.d/custom.el ./.emacs.d/custom.el
